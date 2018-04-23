9e0d7f0c06a5c6bc25f824238174c9abc6f03b6f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/TestProject9.3/Window1.xaml.vb
@@ -0,0 +1,30 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports DevExpress.Xpf.Editors
+Imports DevExpress.Xpf.Core.Native
+Imports System.Windows.Controls.Primitives
+Imports System.Windows.Markup
+Imports System.Windows.Automation.Peers
+Imports System.Windows.Automation.Provider
+Imports System.Collections
+
+Namespace TestProject9._3
+	Partial Public Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+End Namespace
